3375d83f8ec1bb2509a64e4c1a336b3fc84392f1
Zuhriya29 <113580042+Zuhriya29@users.noreply.github.com>
2024-11-08 13:44:22 +0700

    post file

--- e-commerce toko kue/session.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    if($_SESSION['login']==false){
-        header('location: register/login.php');
-    }
-?>
--- e-commerce toko kue/style-js.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const warning = document.querySelector(".warning");
-    if (warning) {
-        warning.style.display = "block";
-        setTimeout(() => {
-            warning.style.display = "none";
-        }, 5000); // 3 detik
-    }
-});
